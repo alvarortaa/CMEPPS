ac6d63518f7f007ccd7ce43e661751d164e6a66e
Alvarortagomez <alvarortaaol@gmail.com>
2023-10-17 12:12:16 +0200

    Test de regresion

--- Sesion3z/src/test/CuentaTest.java
+++ Sesion3z/src/test/CuentaTest.java
@@ -13,11 +13,16 @@ import pkg.Cuenta;
 class CuentaTest {
 	
 	static Cuenta ctaPruebas;
+	static Cuenta cta12345;
+	static Cuenta cta67890;
 	double aux;
+	
 	
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
-		
+		ctaPruebas = new Cuenta("0","Alvaro",0);
+		cta12345 = new Cuenta("12345", "Pepe",50);
+		cta67890 = new Cuenta("67890","Juan",0);
 	}
 
 	@AfterAll
@@ -35,8 +40,8 @@ class CuentaTest {
 	
 	@Test
 	void testRetirar() {
-		ctaPruebas.retirar(3000);
-		assertEquals(aux-3000,ctaPruebas.getSaldo());
+		ctaPruebas.retirar(1);
+		assertEquals(aux-1,ctaPruebas.getSaldo());
 	}
 	@Test
 	void testIngresar() {
@@ -48,6 +53,25 @@ class CuentaTest {
 		ctaPruebas.retirar(100000);
 		
 		assertEquals(aux,ctaPruebas.getSaldo());
+	}
+	@Test
+	void test0014() {
+		
+		cta12345.retirar(200);
+		
+		cta67890.retirar(350);
+		cta12345.ingresar(100);
+		cta67890.retirar(200);
+		cta67890.retirar(150);
+		cta12345.retirar(200);
+		cta67890.ingresar(50);
+		cta67890.retirar(100);
+		
+		
+		assertEquals(-250, cta12345.getSaldo());
+		
+		assertEquals(-450, cta67890.getSaldo());
+		
 	}
 
 }
